2e3ca76d42c84e771f49ef3bae6f96f13bdf3003
coarf <coarf@hotmail.com>
2023-03-31 08:45:31 -0500

    probando categoria

--- src/main/java/com/eamapp/inventory/model/Category.java
+++ src/main/java/com/eamapp/inventory/model/Category.java
@@ -2,6 +2,7 @@ package com.eamapp.inventory.model;
 
 import jakarta.persistence.*;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Entity
@@ -9,7 +10,10 @@ import java.io.Serializable;
         name = "category",
         indexes = {@Index(name = "index_name", columnList = "name", unique = true)})
 public class Category implements Serializable{
+    @Serial
+    private static final long serialVersionUID = 7138772029798401102L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
 }
