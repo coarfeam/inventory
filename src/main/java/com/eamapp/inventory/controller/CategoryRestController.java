d70bda140b225cbb4e60e6a5c8cd757eddc4bf65
coarf <coarf@hotmail.com>
2023-03-31 22:47:10 -0500

    agregar cross para conexión desde angular

--- src/main/java/com/eamapp/inventory/controller/CategoryRestController.java
+++ src/main/java/com/eamapp/inventory/controller/CategoryRestController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1/")
 public class CategoryRestController {
